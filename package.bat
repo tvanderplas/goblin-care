89437db4ca824d8db8bdc92d88cdec94715cb1fb
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2019-03-15 04:21:22 -0500

    create package script

---
+++ package.bat
@@ -0,0 +1,11 @@
+cd %~dp0
+mkdir "temp pyinstaller build"
+cd "temp pyinstaller build"
+set PATH=%PATH%;C:\Windows\System32\downlevel
+pyinstaller "C:\repos\goblin-care\goblin care.py"
+cd..
+xcopy "temp pyinstaller build\dist\goblin care" "Goblin Care" /E /Y /I
+xcopy "game art" "Goblin Care\game art" /Y /I
+rd /s /q "temp pyinstaller build"
+7z a -o. "Goblin Care.zip" -r "goblin care"
+rd /s /q "Goblin Care"
