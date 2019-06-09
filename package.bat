7910f385d0391250aae53d4b3e6e25553474c066
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2019-06-08 21:17:28 -0400

    add fonts to package script

--- package.bat
+++ package.bat
@@ -6,6 +6,7 @@ pyinstaller "C:\repos\goblin-care\goblin care.py"
 cd..
 xcopy "temp pyinstaller build\dist\goblin care" "Goblin Care" /E /Y /I
 xcopy "game art" "Goblin Care\game art" /Y /I
+xcopy "fonts" "Goblin Care\fonts" /Y /I
 rd /s /q "temp pyinstaller build"
 7z a -o. "Goblin Care.zip" -r "goblin care"
 rd /s /q "Goblin Care"
