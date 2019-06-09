b36e9bda1f2fdc15bc2bc0ffb017349ade680481
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2019-06-09 08:05:36 -0400

    add close button to loot window

--- ui.py
+++ ui.py
@@ -8,10 +8,16 @@ import screen
 from sprites import image_path
 
 class Element():
-	def __init__(self, location, size, color=(0, 0, 0)):
+	def __init__(self, location, size, color=(0, 0, 0), orientation='center'):
 		self.surface = pg.Surface(size) # pylint: disable=too-many-function-args
 		self.rect = self.surface.fill(color)
 		self.rect.left, self.rect.top = location
+		if orientation == 'center':
+			self.rect.center = location
+		elif orientation == 'topright':
+			self.rect.topright = location
+		elif orientation == 'topleft':
+			self.rect.topleft = location
 
 class Text():
 	def __init__(self, text, location, size, orientation='center', color=(170, 64, 78)):
@@ -23,12 +29,35 @@ class Text():
 			self.rect.midleft = location
 
 class Icon():
-	def __init__(self, location, size, image_file):
+	def __init__(self, location, size, image_file, orientation='center'):
 		self.surface = pg.image.load(image_path + image_file).convert()
 		self.surface.set_colorkey((255, 255, 255), RLEACCEL)
 		self.surface = pg.transform.scale(self.surface, size)
 		self.rect = self.surface.get_rect()
-		self.rect.left, self.rect.top = location
+		if orientation == 'center':
+			self.rect.center = location
+		elif orientation == 'topright':
+			self.rect.topright = location
+		elif orientation == 'topleft':
+			self.rect.topleft = location
+
+class Button():
+	def __init__(self, location, size, image_file, orientation='center'):
+		icon = Icon(location, size, image_file, orientation)
+		self.surface, self.rect = icon.surface, icon.rect
+		self.is_hovering = False
+	def rollover(self):
+		over_x = self.rect.left <= pg.mouse.get_pos()[0] <= self.rect.right
+		over_y = self.rect.top <= pg.mouse.get_pos()[1] <= self.rect.bottom
+		return over_x and over_y
+	def hover(self):
+		if self.rollover() and not self.is_hovering:
+			# highlight red
+			self.is_hovering = True
+		if not self.rollover() and self.is_hovering:
+			# remove highlight
+			self.is_hovering = False
+
 
 class Window():
 	def __init__(self, title, splat_count, location=(0, 0), size=(screen.width, screen.height)):
@@ -39,12 +68,19 @@ class Window():
 		self.rect = self.surface.fill((21, 26, 27))
 		self.rect.left, self.rect.top = location
 
-		self.title_bar = Element(location, (size[0], screen.height // 30), (65, 65, 65))
+		self.title_bar = Element(location, (size[0], screen.height // 30), (65, 65, 65), 'topleft')
 		self.title = Text(title, self.title_bar.rect.center, self.title_bar.rect.height * 7 // 8)
+		self.close_button = Button(
+			(screen.width, 0),
+			(self.title_bar.rect.height, self.title_bar.rect.height),
+			'window close.png',
+			orientation='topright'
+		)
 		self.splat_icon = Icon(
 			(screen.width // 50, screen.height * 11 // 12),
 			(screen.width // 25, screen.height // 16),
-			'splat.png'
+			'splat.png',
+			orientation='topleft'
 		)
 		self.splat_count = Text(
 			str(splat_count),
@@ -52,19 +88,19 @@ class Window():
 			screen.height // 16,
 			orientation='midleft'
 		)
-
 		self.is_open = True
 	def open(self):
 		while self.is_open:
 			for event in pg.event.get():
 				if (event.type == KEYDOWN and event.key == K_ESCAPE) or event.type == QUIT:
 					self.is_open = False
-				# if event.type == MOUSEBUTTONDOWN and self.quit_button.rollover():
-					# 	self.is_open = False
+				if event.type == MOUSEBUTTONDOWN and self.close_button.rollover():
+						self.is_open = False
 			pg.display.flip()
 			self.view.blit(self.surface, self.rect)
 			self.view.blit(self.title_bar.surface, self.title_bar.rect)
 			self.view.blit(self.title.surface, self.title.rect)
+			self.view.blit(self.close_button.surface, self.close_button.rect)
 			self.view.blit(self.splat_icon.surface, self.splat_icon.rect)
 			self.view.blit(self.splat_count.surface, self.splat_count.rect)
 			self.clock.tick(60)
