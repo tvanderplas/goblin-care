f3da8c86e93b2ebe14cda3c775fbeb2dca1fda87
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2019-06-09 01:05:41 -0400

    change main game from function to class

--- goblin care.py
+++ goblin care.py
@@ -26,66 +26,69 @@ tornados = pg.sprite.Group()
 all_sprites = pg.sprite.Group()
 # pylint: enable=no-member
 
-inv_button_location = (screen.width // 100, screen.height * 9 // 10)
-inv_button_size = (screen.width // 19, screen.height // 12)
-inv_button = Game_UI_Button('treasure.png', inv_button_location, inv_button_size)
+class Game():
+	def __init__(self):
+		self.player = Player()
+		all_sprites.add(self.player)
 
-def game():
-	player = Player()
-	all_sprites.add(player)
-	all_sprites.add(inv_button)
-	isRunning = True
-	while isRunning:
+		inv_button_location = (screen.width // 100, screen.height * 9 // 10)
+		inv_button_size = (screen.width // 19, screen.height // 12)
+		self.inv_button = Game_UI_Button('treasure.png', inv_button_location, inv_button_size)
+		all_sprites.add(self.inv_button)
 
-		if pg.sprite.spritecollideany(player, enemies):
+		self.isRunning = True
+	def play(self):
+		while self.isRunning:
+
+			if pg.sprite.spritecollideany(self.player, enemies):
+				for sprite in all_sprites:
+					sprite.kill()
+				self.isRunning = False
+			for enemy in pg.sprite.groupcollide(enemies, bullets, True, True):
+				new_splat = Splat(enemy.rect.centerx, enemy.rect.centery)
+				all_sprites.add(new_splat)
+				splats.add(new_splat)
+			for tornado in pg.sprite.spritecollide(self.player, tornados, False):
+				if tornado.magic < 8:
+					for sprite in all_sprites:
+						sprite.kill()
+					self.isRunning = False
+				else:
+					self.player.embiggen()
+
+			for event in pg.event.get():
+				if (event.type == KEYDOWN and event.key == K_ESCAPE) or event.type == QUIT:
+					for sprite in all_sprites:
+						sprite.kill()
+					self.isRunning = False
+				elif event.type == ADDENEMY:
+					new_enemy = Enemy()
+					enemies.add(new_enemy)
+					all_sprites.add(new_enemy)
+				elif event.type == NEWTORNADO:
+					new_tornado = Tornado()
+					tornados.add(new_tornado)
+					all_sprites.add(new_tornado)
+				elif event.type == KEYDOWN and event.key == K_SPACE:
+					new_player_bullet = PlayerBullet(self.player.rect.right, self.player.rect.centery)
+					all_sprites.add(new_player_bullet)
+					bullets.add(new_player_bullet)
+
 			for sprite in all_sprites:
-				sprite.kill()
-			isRunning = False
-		for enemy in pg.sprite.groupcollide(enemies, bullets, True, True):
-			new_splat = Splat(enemy.rect.centerx, enemy.rect.centery)
-			all_sprites.add(new_splat)
-			splats.add(new_splat)
-		for tornado in pg.sprite.spritecollide(player, tornados, False):
-			if tornado.magic < 8:
-				for sprite in all_sprites:
-					sprite.kill()
-				isRunning = False
-			else:
-				player.embiggen()
-
-		for event in pg.event.get():
-			if (event.type == KEYDOWN and event.key == K_ESCAPE) or event.type == QUIT:
-				for sprite in all_sprites:
-					sprite.kill()
-				isRunning = False
-			elif event.type == ADDENEMY:
-				new_enemy = Enemy()
-				enemies.add(new_enemy)
-				all_sprites.add(new_enemy)
-			elif event.type == NEWTORNADO:
-				new_tornado = Tornado()
-				tornados.add(new_tornado)
-				all_sprites.add(new_tornado)
-			elif event.type == KEYDOWN and event.key == K_SPACE:
-				new_player_bullet = PlayerBullet(player.rect.right, player.rect.centery)
-				all_sprites.add(new_player_bullet)
-				bullets.add(new_player_bullet)
-
-		for sprite in all_sprites:
-			sprite.update()
-		for splat in splats:
-			view.blit(splat.surface, splat.rect)
-		view.blit(player.surface, player.rect)
-		for enemy in enemies:
-			view.blit(enemy.surface, enemy.rect)
-		for bullet in bullets:
-			view.blit(bullet.surface, bullet.rect)
-		for tornado in tornados:
-			view.blit(tornado.surface, tornado.rect)
-		view.blit(inv_button.surface, inv_button.rect)
-		pg.display.flip()
-		view.blit(background.surface, background.rect)
-		clock.tick(60)
+				sprite.update()
+			for splat in splats:
+				view.blit(splat.surface, splat.rect)
+			view.blit(self.player.surface, self.player.rect)
+			for enemy in enemies:
+				view.blit(enemy.surface, enemy.rect)
+			for bullet in bullets:
+				view.blit(bullet.surface, bullet.rect)
+			for tornado in tornados:
+				view.blit(tornado.surface, tornado.rect)
+			view.blit(self.inv_button.surface, self.inv_button.rect)
+			pg.display.flip()
+			view.blit(background.surface, background.rect)
+			clock.tick(60)
 
 menu_screen = Background('menu.png', [0, 0])
 play_button = Menu_Button('Play!', (-screen.width // 5, screen.height // 2))
@@ -99,7 +102,8 @@ while True:
 			raise SystemExit
 		if event.type == MOUSEBUTTONDOWN:
 			if play_button.rollover():
-				game()
+				game = Game()
+				game.play()
 			elif quit_button.rollover():
 				raise SystemExit
 	pg.display.flip()
