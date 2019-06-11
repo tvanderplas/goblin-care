2a62c0e4b4610611e9798291e327c378175e9b20
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2019-06-10 23:57:33 -0400

    improve, rename utility script

--- game art/color find replace.py
+++ game art/color find replace.py
@@ -3,8 +3,12 @@ import sys
 from PIL import Image
 import numpy as np
 
+find_color = [255, 0, 0, 255]
+replace_color = [255, 255, 255, 255]
+
 source = Image.open(sys.argv[-1])
 target = np.array(source)
-target[target > 230] = 255
+mask = np.all(target == find_color, axis=-1)
+target[mask] = replace_color
 target = Image.fromarray(target)
 target.save('corrected.png', 'PNG')
