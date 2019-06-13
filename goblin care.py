8669b7b03fffdfcba163c6252374a082af7dbb8f
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2019-06-13 02:05:17 -0400

    add version check on launch

--- goblin care.py
+++ goblin care.py
@@ -1,6 +1,16 @@
 
 import pygame as pg
 from menu import Menu
+import requests
+
+current_version = 'v0.3.0'
+url = 'https://api.github.com/repos/tvanderplas/goblin-care/releases'
+r = requests.get(url)
+latest = r.json()[0]['tag_name']
+if current_version is latest:
+	print('Up to date')
+else:
+	print('Not up to date')
 
 pg.init() # pylint: disable=no-member
 
