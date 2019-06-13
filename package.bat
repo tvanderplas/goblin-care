8385edd7c83314dfbc3638cb54d83354e5cea92d
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2019-06-12 22:35:49 -0400

    update package script to create windows installer

--- package.bat
+++ package.bat
@@ -1,3 +1,4 @@
+
 cd %~dp0
 mkdir "temp pyinstaller build"
 cd "temp pyinstaller build"
@@ -8,5 +9,8 @@ xcopy "temp pyinstaller build\dist\goblin care" "Goblin Care" /E /Y /I
 xcopy "game art" "Goblin Care\game art" /Y /I
 xcopy "fonts" "Goblin Care\fonts" /Y /I
 rd /s /q "temp pyinstaller build"
-7z a -o. "Goblin Care.zip" -r "goblin care"
+candle.exe "goblin care.wxs"
+light.exe "goblin care.wixobj"
 rd /s /q "Goblin Care"
+del "goblin care.wixobj"
+del "goblin care.wixpdb"
