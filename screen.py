1fee546ab013aa86b471e32b6d59311a5538a703
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2019-06-15 13:07:06 -0400

    hide ctypes behind platform condition

--- screen.py
+++ screen.py
@@ -1,9 +1,16 @@
 
-from ctypes import windll
+import platform
+if platform.system() == 'Windows':
+	from ctypes import windll
 
-user32 = windll.user32
-user32.SetProcessDPIAware()
-"""prevents stretching"""
+	user32 = windll.user32
+	user32.SetProcessDPIAware() # prevents stretching
+	width = user32.GetSystemMetrics(0)
+	height = user32.GetSystemMetrics(1)
 
-width = user32.GetSystemMetrics(0)
-height = user32.GetSystemMetrics(1)
+if platform.system() == 'Linux':
+	width = 1920
+	height = 1080
+if platform.system() == 'Darwin':
+	width = 1920
+	height = 1080
