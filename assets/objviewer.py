fea0911858411ce59fbc9dbaa2a12c9dacdf4bba
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2019-08-05 18:52:49 -0400

    use new functionality to render

--- assets/objviewer.py
+++ assets/objviewer.py
@@ -13,25 +13,14 @@ def main():
 	pygame.display.set_mode(display, DOUBLEBUF|OPENGL) # pylint: disable=undefined-variable
 	glEnable(GL_DEPTH_TEST)
 
-	player = objloader.Obj('sedan_uvmap.obj', 'car body.vs', 'car body.fs')
-	player.generate()
-	glass = objloader.Obj('sedan_glass.obj', 'car body.vs', 'car body.fs')
-	glass.generate()
 	light_cube = objloader.Obj('cube.obj', 'basic vertex shader.vs', 'basic fragment shader.fs')
 	light_cube.generate()
-
-	car_vertex_shader = shaders.compileShader(open('car body.vs'), GL_VERTEX_SHADER)
-	car_fragment_shader = shaders.compileShader(open('car body.fs'), GL_FRAGMENT_SHADER)
-	car_shader = shaders.compileProgram(car_vertex_shader, car_fragment_shader)
-
-	UNIFORM_LOCATIONS = {
-		'model': glGetUniformLocation(car_shader, 'model'),
-		'transform': glGetUniformLocation(car_shader, 'transform'),
-		'light_color': glGetUniformLocation(car_shader, 'light_color'),
-		'light_position': glGetUniformLocation(car_shader, 'light_position')
-	}
-	shaders.glUseProgram(car_shader) # pylint: disable=no-member
-	glUniform3fv(UNIFORM_LOCATIONS['light_color'], 1, (1,1,1))
+	player = objloader.Obj('sedan_uvmap.obj', 'car body.vs', 'car body.fs')
+	player.generate()
+	player.set_light_source(light_cube)
+	glass = objloader.Obj('sedan_glass.obj', 'car body.vs', 'car body.fs')
+	glass.generate()
+	glass.set_light_source(light_cube)
 
 	objloader.set_perspective(pi / 4, *display, 0.1, 100)
 	light_cube.translate(10, 5, -25)
@@ -48,20 +37,11 @@ def main():
 			if (event.type == KEYDOWN and event.key == K_ESCAPE) or event.type == QUIT: # pylint: disable=undefined-variable
 				raise SystemExit
 
-		shaders.glUseProgram(car_shader) # pylint: disable=no-member
 		player.rotate(pi / 1000, 0, 0, 1)
-		glUniformMatrix4fv(UNIFORM_LOCATIONS['transform'], 1, False, player.model * player.perspective)
-		glUniform3f(UNIFORM_LOCATIONS['light_position'], *light_cube.position)
-		glUniformMatrix4fv(UNIFORM_LOCATIONS['model'], 1, False, player.model)
-		glBindVertexArray(player.VAO)
-		glDrawElements(GL_TRIANGLES, len(player.indices), GL_UNSIGNED_INT, None)
+		player.draw()
 
 		glass.rotate(pi / 1000, 0, 0, 1)
-		glUniformMatrix4fv(UNIFORM_LOCATIONS['transform'], 1, False, glass.model * glass.perspective)
-		glUniform3f(UNIFORM_LOCATIONS['light_position'], *light_cube.position)
-		glUniformMatrix4fv(UNIFORM_LOCATIONS['model'], 1, False, glass.model)
-		glBindVertexArray(glass.VAO)
-		glDrawElements(GL_TRIANGLES, len(glass.indices), GL_UNSIGNED_INT, None)
+		glass.draw()
 
 		light_cube.rotate(pi / 100, 0, -1, 0)
 		light_cube.draw()
