a0b5ed0c162364542c821455951128e7c26faeed
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2019-08-07 13:20:49 -0400

    add paths to get absolute paths of files

---
+++ assets/paths.py
@@ -0,0 +1,15 @@
+
+from pywavefront import Wavefront
+from os.path import dirname, realpath, join
+dir_path = dirname(realpath(__file__))
+
+cube_obj = join(dir_path, 'cube.obj')
+light_fs = join(dir_path, 'light.fs')
+light_vs = join(dir_path, 'light.vs')
+menu_png = join(dir_path, 'menu.png')
+object_fs = join(dir_path, 'object.fs')
+object_vs = join(dir_path, 'object.vs')
+sedan_body_obj = join(dir_path, 'sedan_body.obj')
+sedan_glass_obj = join(dir_path, 'sedan_glass.obj')
+square_obj = join(dir_path, 'square.obj')
+uv_test_png = join(dir_path, 'uv_test.png')
