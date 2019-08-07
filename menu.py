6345ad1ee78c593b46cf2bef4352130c57e385d7
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2019-08-07 14:03:50 -0400

    put new buttons where the old ones were

--- menu.py
+++ menu.py
@@ -16,21 +16,13 @@ class Menu_Button(objloader.Obj):
 		size = (screen.width // 3, screen.height // 20)
 		img = Image.new('RGB', size, color = (65, 65, 65))
 		font_ = ImageFont.truetype('fonts/calibri.ttf', 15)
-		d = ImageDraw.Draw(img)
-		d.text((10,10), text, font=font_, fill=(170, 64, 78))
-		super().__init__(square_obj, object_vs, object_fs, d)
+		decoration = ImageDraw.Draw(img)
+		decoration.text((10,10), text, font=font_, fill=(170, 64, 78))
+		super().__init__(square_obj, object_vs, object_fs, decoration)
 		super().generate()
+		super().translate(*location, 0)
+		super().scale(.33, .05, 1)
 
-		# self.surface = pg.Surface(size) # pylint: disable=too-many-function-args
-		# self.rect = self.surface.fill((65, 65, 65))
-		# self.text = ft.Font('fonts/calibri.ttf', size=size[1] * 7 // 8)
-		# self.text.rect = self.text.get_rect(text)
-		# offset = (self.rect.height - self.text.rect.height) // 2
-		# text_location = ((self.rect.right - self.text.rect.width) - offset, self.rect.top + offset)
-		# self.text.rect = self.text.render_to(
-		# 	self.surface, text_location, text, fgcolor=(170, 64, 78)
-		# )
-		# self.rect.left, self.rect.top = location
 		# self.is_hovering = False
 	def rollover(self):
 		return False # self.rect.top < pg.mouse.get_pos()[1] < self.rect.bottom
@@ -47,8 +39,8 @@ class Menu():
 		self.clock = pg.time.Clock()
 		self.view = pg.display.set_mode((screen.width, screen.height), FULLSCREEN|OPENGL|DOUBLEBUF)
 		self.menu_screen = Background(menu_png)
-		self.play_button = Menu_Button('Play!', (-screen.width // 5, screen.height // 2))
-		self.quit_button = Menu_Button('Quit', (-screen.width // 5, screen.height * 3 // 5))
+		self.play_button = Menu_Button('Play!', (-1, 0))
+		self.quit_button = Menu_Button('Quit', (-1, -.2))
 	def play(self):
 		while True:
 			# self.play_button.hover()
@@ -64,6 +56,6 @@ class Menu():
 						raise SystemExit
 			pg.display.flip()
 			self.menu_screen.draw()
-			# self.play_button.draw()
-			# self.quit_button.draw()
+			self.play_button.draw()
+			self.quit_button.draw()
 			self.clock.tick(60)
