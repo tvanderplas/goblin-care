aff3e627e3bcb7a63c757d3c5abcd578c9aa35b2
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2019-08-11 00:32:52 -0400

    get rollover behavior working

--- menu.py
+++ menu.py
@@ -11,6 +11,11 @@ from PIL import Image, ImageDraw, ImageFont
 from assets import objloader
 from assets.paths import square_obj, object_vs, object_fs, Menu_Button_png, menu_png
 
+def pixel_to_view(x, y):
+	vx = x / (screen.width / 2) - 1
+	vy = y / (screen.height / 2) - 1
+	return (-vx, -vy)
+
 class Menu_Button(objloader.Obj):
 	def __init__(self, text:str, location:tuple):
 		size = (screen.width // 3, screen.height // 20)
@@ -22,21 +27,23 @@ class Menu_Button(objloader.Obj):
 		img.save(Menu_Button_png)
 
 		super().__init__(square_obj, object_vs, object_fs, Menu_Button_png)
-		super().generate()
-		super().translate(*location, 0)
-		super().scale(.33, .05, 1)
-		super().set_texture(1)
+		self.generate()
+		self.translate(*location, 0)
+		self.scale(.33, .05, 1)
+		self.set_texture(1)
 
-		# self.is_hovering = False
+		self.is_hovering = False
+
 	def rollover(self):
-		return False # self.rect.top < pg.mouse.get_pos()[1] < self.rect.bottom
-	# def hover(self):
-	# 	if self.rollover() and not self.is_hovering:
-	# 		self.rect.move_ip(screen.width // 30, 0)
-	# 		self.is_hovering = True
-	# 	if not self.rollover() and self.is_hovering:
-	# 		self.rect.move_ip(-screen.width // 30, 0)
-	# 		self.is_hovering = False
+		return self.box.ly < pixel_to_view(*pg.mouse.get_pos())[1] < self.box.uy
+
+	def hover(self):
+		if self.rollover() and not self.is_hovering:
+			self.translate(1 / 30, 0, 0)
+			self.is_hovering = True
+		if not self.rollover() and self.is_hovering:
+			self.translate(-1 / 30, 0, 0)
+			self.is_hovering = False
 
 class Menu():
 	def __init__(self):
@@ -47,8 +54,6 @@ class Menu():
 		self.quit_button = Menu_Button('Quit', (-1, -.2))
 	def play(self):
 		while True:
-			# self.play_button.hover()
-			# self.quit_button.hover()
 			for event in pg.event.get():
 				if (event.type == KEYDOWN and event.key == K_ESCAPE) or event.type == QUIT:
 					raise SystemExit
@@ -60,6 +65,8 @@ class Menu():
 						raise SystemExit
 			pg.display.flip()
 			self.menu_screen.draw()
+			self.play_button.hover()
 			self.play_button.draw()
+			self.quit_button.hover()
 			self.quit_button.draw()
 			self.clock.tick(60)
