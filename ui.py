33ee7b21a1d851ac69da26df5e41704de410080e
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2019-08-13 20:44:31 -0400

    window close button now closes window, remove useless code

--- ui.py
+++ ui.py
@@ -11,40 +11,6 @@ from fonts.paths import calibri_ttf
 from assets import objloader
 from helpers import text_image, pixel_to_view
 
-class Element():
-	def __init__(self, location, size, color=(0, 0, 0), orientation='center'):
-		self.surface = pg.Surface(size) # pylint: disable=too-many-function-args
-		self.rect = self.surface.fill(color)
-		self.rect.left, self.rect.top = location
-		if orientation == 'center':
-			self.rect.center = location
-		elif orientation == 'topright':
-			self.rect.topright = location
-		elif orientation == 'topleft':
-			self.rect.topleft = location
-
-class Text():
-	def __init__(self, text, location, size, orientation='center', color=(170, 64, 78)):
-		self.font = ft.Font(calibri_ttf, size=size)
-		self.surface, self.rect = self.font.render(text, fgcolor=color)
-		if orientation == 'center':
-			self.rect.center = location
-		elif orientation == 'midleft':
-			self.rect.midleft = location
-
-class Icon():
-	def __init__(self, location, size, image_file, orientation='center', color=(255, 255, 255)):
-		self.surface = pg.image.load(image_file).convert()
-		self.surface.set_colorkey(color, RLEACCEL)
-		self.surface = pg.transform.scale(self.surface, size)
-		self.rect = self.surface.get_rect()
-		if orientation == 'center':
-			self.rect.center = location
-		elif orientation == 'topright':
-			self.rect.topright = location
-		elif orientation == 'topleft':
-			self.rect.topleft = location
-
 class Close_Button:
 	def __init__(self):
 		self.close_button = objloader.Obj(square_obj, object_vs, object_fs, window_close_png)
@@ -102,18 +68,6 @@ class Window:
 
 		self.close_button = Close_Button()
 
-		self.splat_icon = Icon(
-			(screen.width // 50, screen.height * 11 // 12),
-			(screen.width // 25, screen.height // 16),
-			splat_png,
-			orientation='topleft'
-		)
-		self.splat_count = Text(
-			str(splat_count),
-			(screen.width // 15, screen.height * 19 // 20),
-			screen.height // 16,
-			orientation='midleft'
-		)
 		self.is_open = True
 	def draw(self):
 		self.background.draw()
@@ -122,22 +76,15 @@ class Window:
 		self.close_button.draw()
 	def open(self):
 		while self.is_open:
-			# self.close_button.hover()
 			for event in pg.event.get():
 				if (
 					(event.type == KEYDOWN and event.key == K_ESCAPE) or
 					(event.type == KEYDOWN and event.key == K_TAB) or
-					# (event.type == MOUSEBUTTONDOWN and self.close_button.rollover()) or
+					(event.type == MOUSEBUTTONDOWN and self.close_button.rollover()) or
 					event.type == QUIT
 				):
 					self.is_open = False
 			pg.display.flip()
 			glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 			self.draw()
-			# self.view.blit(self.surface, self.rect)
-			# self.view.blit(self.title_bar.surface, self.title_bar.rect)
-			# self.view.blit(self.title.surface, self.title.rect)
-			# self.view.blit(self.close_button.surface, self.close_button.rect)
-			# self.view.blit(self.splat_icon.surface, self.splat_icon.rect)
-			# self.view.blit(self.splat_count.surface, self.splat_count.rect)
 			self.clock.tick(60)
