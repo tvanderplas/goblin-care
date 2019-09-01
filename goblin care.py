e3197109689f2ec283f1d46cd913bc830a22b7e1
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2019-09-01 19:03:23 -0400

    add error logging

--- goblin care.py
+++ goblin care.py
@@ -1,9 +1,16 @@
 
 import pygame as pg
 from menu import Menu
+import traceback as tb
 
 pg.init() # pylint: disable=no-member
 
 if __name__ == '__main__':
 	menu = Menu()
-	menu.play()
+	try:
+		menu.play()
+	except Exception as error_:
+		traceback_ = tb.format_list(tb.extract_tb(error_.__traceback__)) + [str(error_)]
+		error_log = open('error.log', 'w')
+		error_log.write(''.join(traceback_))
+		error_log.close()
