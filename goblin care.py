46d144b5afad097ce1acb754663ed3dfb9da0601
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2020-04-27 10:56:47 -0400

    append error log instead of overwrite

--- goblin care.py
+++ goblin care.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
 		menu.play()
 	except Exception as error_:
 		traceback_ = tb.format_list(tb.extract_tb(error_.__traceback__)) + [str(error_)]
-		error_log = open('error.log', 'w')
-		error_log.write(''.join(traceback_))
+		error_log = open('error.log', 'a')
+		error_log.writelines(['\n\n'] + traceback_)
 		error_log.close()
