5036b21b23923c829f82158e259bde7af2910275
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2020-05-01 02:14:51 -0400

    put DPI aware code in try block instead of checking os

--- screen.py
+++ screen.py
@@ -1,13 +1,12 @@
 
-import os
 from screeninfo import get_monitors
 
-if os.name == "Windows":
+try:
     from ctypes import windll
-
-    user32 = windll.user32
-    user32.SetProcessDPIAware()
-    """prevents stretching"""
+    """prevents stretching on windows"""
+    windll.user32.SetProcessDPIAware()
+except:
+    pass
 
 width = get_monitors()[0].width
 height = get_monitors()[0].height
