8c8bca64e181599ff76b8ceb47d9d35ffe79f145
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2020-05-02 02:25:15 -0400

    update utility script

--- utility/color find replace.py
+++ utility/color find replace.py
@@ -8,11 +8,9 @@ from assets.paths import tornado_png, rainbow_tornado_png
 find_color = [255, 255, 255, 255]
 replace_color = [255, 255, 255, 0]
 
-source = Image.open(tornado_png)
+source = Image.open(tornado_png).convert('RGBA')
 target = np.array(source)
-if target.shape[2] == 3:
-	alpha = np.array([[[255] * 112] * 100]).reshape(100, 112, 1)
-	target = np.insert(target, 3, 255, axis=2)
+
 mask = np.all(target == find_color, axis=-1)
 target[mask] = replace_color
 target = Image.fromarray(target)
