3b1a57a350ffcf48dddea7e3f4d8f358e1255dac
tvanderplas <36511661+tvanderplas@users.noreply.github.com>
2020-05-04 11:22:58 -0400

    refactor object.fs

--- shaders/object.fs
+++ shaders/object.fs
@@ -3,12 +3,12 @@
 
 precision mediump float;
 
-out vec4 fragment_color;
-
 in vec3 position;
 in vec3 normal;
 in vec4 color;
 in vec2 texture_coord;
+
+out vec4 fragment_color;
 
 uniform vec3 light_color;
 uniform vec3 light_position;
@@ -21,17 +21,18 @@ void main()
 	float ambientStrength = 0.1;
 	vec3 ambient = ambientStrength * light_color;
 
+	// diffuse
 	vec3 light_direction = normalize(light_position - position);
 	float diff = max(dot(normal, light_direction), 0.0);
 	vec3 diffuse = diff * light_color;
+
 	if (texture_mode == 0) {
-		vec4 result = vec4(ambient + diffuse, 1.0) * color;
-		fragment_color = result;
+		vec4 diffuse_color = vec4(ambient + diffuse, 1.0) * color;
+		fragment_color = diffuse_color;
 	} else {
 		vec4 tex_color = texture(textureObj, texture_coord);
-		vec4 result = vec4(ambient + diffuse, 1.0) * tex_color;
 		if(tex_color.a == 0.0)
 			discard;
-		fragment_color = result;
+		fragment_color = vec4(ambient + diffuse, 1.0) * tex_color;
 	}
 }
